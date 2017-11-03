9ca809f605bafdf6a8bbe3f8bb76e6289b6e74b1
DK\obr <obr@sitecore.net>
2017-11-03 11:32:30 +0200

    Init

---
+++ src/Sitecore.Support.191398/PackagingEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.ContentSearch.Maintenance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.Events
+{
+    public class PackagingEventHandler
+    {
+        public void OnPackageInstallStartingHandler(object sender, EventArgs e)
+        {
+            //Sitecore.Support.191398
+            //'PauseIndexing' is used instaed of 'StopIndexing' as opposed to the original one.
+            CrawlingLog.Log.Warn("Pausing indexing while package is being installed.", null);            
+            IndexCustodian.PauseIndexing();            
+        }
+    }
+}
