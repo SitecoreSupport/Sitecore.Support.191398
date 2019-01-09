d3a5afcc3f48093a7c27bee23438a3d4b6e50feb
gpo <gpo@sitecore.net>
2019-01-09 12:36:21 -0500

    First commit

---
+++ src/Sitecore.Support.191398/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.191398")]
+[assembly: AssemblyProduct("Sitecore.Support.191398")]
+[assembly: ComVisible(false)]
